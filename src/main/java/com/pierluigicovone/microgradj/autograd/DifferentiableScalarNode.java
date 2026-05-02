bc4a1341b2fbee6d4243988f5979ae767905fb5d
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-05-02 14:23:19 +0200

    Rename the "GradScalar" class as "DifferentiableScalarNode"

---
+++ src/main/java/com/pierluigicovone/microgradj/autograd/DifferentiableScalarNode.java
@@ -0,0 +1,60 @@
+package com.pierluigicovone.microgradj.autograd;
+
+/**
+ * Building Block of the automatic differentiation engine.
+ *
+ * Wraps a scalar value and tracks the operation that produced it,
+ * along with references to its parent nodes in the computation graph.
+ *
+ * Each node knows how to propagate its gradient backward to its parents,
+ * enabling reverse-mode automatic differentiation.
+ */
+public class DifferentiableScalarNode {
+
+    // --- FIELDS ---
+    private final double data;      // <Is it really final???>
+    private double grad;
+
+
+    // --- METHODS ---
+
+    /**
+     * Takes as parameter in input the scalar value "data" (temporarily...).
+     */
+    public DifferentiableScalarNode(double data) {
+        // initialize fields
+        this.data = data;
+    }
+
+    /**
+     * Get the "data" value.
+     */
+    public double getData() {
+        return data;
+    }
+
+    /**
+     * Get the "gradient" value.
+     */
+    public double getGrad() {
+        return grad;
+    }
+
+    /**
+     * Set the gradient.
+     */
+    public void setGrad(double grad) {
+        this.grad = grad;
+    }
+
+
+
+
+    // --- OVERRIDES ---
+
+    @Override
+    public String toString() {
+        return String.format("DifferentiableScalarNode(data=%s, grad=%s)\n", data,grad);
+    }
+
+}
