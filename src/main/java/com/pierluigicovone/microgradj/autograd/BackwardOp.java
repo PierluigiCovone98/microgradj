6ca35ed48936848a2efd7fdfc5af5fd446a07ab3
PierluigiCovone98 <covone.1895077@studenti.uniroma1.it>
2026-05-05 17:54:25 +0200

    Add the "BackwardOp" functional interface to the codebase

---
+++ src/main/java/com/pierluigicovone/microgradj/autograd/BackwardOp.java
@@ -0,0 +1,11 @@
+package com.pierluigicovone.microgradj.autograd;
+
+/**
+ * We use this functional interface as a placeholder,
+ * such that each node can have a field containing its own function
+ * for backpropagation.
+ */
+@FunctionalInterface
+public interface BackwardOp {
+    void propagate();
+}
